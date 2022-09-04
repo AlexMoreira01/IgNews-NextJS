2356b382dba55c5fb6439bec0e5c74b8cd97b10e
Alex Moreira <alex.moreira.andrade@hotmail.com>
2022-09-04 18:42:52 -0300

    Feat: initial settings --  global styles

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, {Html, Head, Main, NextScript} from 'next/document';
+
+export default class MyDocument extends Document{
+    render() {
+        return(
+            <Html>
+                <Head>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+
+                    <title>ig.news</title>
+                </Head>
+                <body>
+                    {/* Substitui a div com id root */}
+                    <Main /> 
+                    {/* Onde o next ira colocar os arquivos js */}
+                    <NextScript/>
+                </body>
+            </Html>
+        )
+    }
+}
